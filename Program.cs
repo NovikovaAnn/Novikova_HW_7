1f9a1229a68a92a224b318a0099783338f8798f7
Anna Novikova <turinovaann@gmail.com>
2022-07-05 19:26:39 +0700

    Minor deletion

--- Program.cs
+++ Program.cs
@@ -48,19 +48,6 @@ PrintTwoDimArray(matrix);
 ArrAvg(matrix);
 */
 
-// int[,] RandomArray(int m, int n);
-// {
-//     int[,] result = new int[m, n];
-//     for (int i = 0; i < m; i++)
-//     {
-//         for (int j = 0; j < n; j++)
-//         {
-//             result[i,j] = new Random().Next(1, 10);
-//         }
-//     }
-// }
-
-
 
 //Задача 47: Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
 /*
